09ebba6d2c90f3aeab559187dfe60be18ff7dc78
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2012-01-23 11:18:38 +1100

    Implement and document basic API calls: #record, #alias, and #set

--- lib/delayable_km.rb
+++ lib/delayable_km.rb
@@ -1,5 +1,12 @@
 require "delayable_km/version"
 
+# delayable_km: A minimal wrapper for the KISSmetrics API that can be used with delayed_job
+#
+# @see https://github.com/kissmetrics/km The official KISSmetrics Ruby gem: km
+# @see http://support.kissmetrics.com/apis/specifications The official KISSmetrics API specification
+# @see http://support.kissmetrics.com/apis/api-tips Tips for using the KISSmetrics API
+# @see http://support.kissmetrics.com/apis/common-methods Commonly used API calls
+# @see http://support.kissmetrics.com/apis/ruby-specific#basic-rails-integration Tips for integrating the KISSmetrics Ruby and Javascript APIs with Rails
 module DelayableKm
-  # Your code goes here...
+  require 'delayable_km/km'
 end
--- lib/delayable_km/version.rb
+++ lib/delayable_km/version.rb
@@ -1,3 +1,5 @@
 module DelayableKm
+  # The version of delayable_km that you're using. delayable_km is versioned using semantic versioning.
+  # @see http://semver.org/
   VERSION = "0.0.1"
 end
