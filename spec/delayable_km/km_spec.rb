2dc319a0b1dce5fa0c1bade0118295f3e1882238
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2012-01-23 11:21:14 +1100

    Stub out test suite

---
+++ spec/delayable_km/km_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe KM do
+  describe '.record' do
+    pending
+  end
+
+  describe '.alias' do
+    pending
+  end
+
+  describe '.set' do
+    pending
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'rspec'
+require 'webmock/rspec'
+
+require 'delayable_km'
